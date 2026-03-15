698c501a4f0f15c7d359efcd8a19d32f30ffa839
Amndeep Singh Mann <amann@mitre.org>
2026-03-15 01:20:29 -0400

    updated oidc
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/backend/src/authn/oidc.strategy.ts
+++ apps/backend/src/authn/oidc.strategy.ts
@@ -44,77 +44,73 @@ export class OidcStrategy extends PassportStrategy(Strategy, 'oidc') {
     private readonly configService: ConfigService,
     private readonly groupsService: GroupsService
   ) {
-    super(
-      {
-        issuer: configService.get('OIDC_ISSUER') || 'disabled',
-        authorizationURL:
-          configService.get('OIDC_AUTHORIZATION_URL') || 'disabled',
-        tokenURL: configService.get('OIDC_TOKEN_URL') || 'disabled',
-        userInfoURL: configService.get('OIDC_USER_INFO_URL') || 'disabled',
-        clientID: configService.get('OIDC_CLIENTID') || 'disabled',
-        clientSecret: configService.get('OIDC_CLIENT_SECRET') || 'disabled',
-        callbackURL: `${configService.get('EXTERNAL_URL')}/authn/oidc_callback`,
-        pkce:
-          configService.get('OIDC_USES_PKCE_S256') === 'true'
-            ? 'S256'
-            : configService.get('OIDC_USES_PKCE_PLAIN') === 'true'
-              ? 'plain'
-              : undefined,
-        scope: ['openid', 'email', 'profile'],
-        skipUserProfile: false,
-        proxy:
-          configService.get('OIDC_USE_HTTPS_PROXY') === 'true'
-            ? true
+    super({
+      issuer: configService.get('OIDC_ISSUER') || 'disabled',
+      authorizationURL:
+        configService.get('OIDC_AUTHORIZATION_URL') || 'disabled',
+      tokenURL: configService.get('OIDC_TOKEN_URL') || 'disabled',
+      userInfoURL: configService.get('OIDC_USER_INFO_URL') || 'disabled',
+      clientID: configService.get('OIDC_CLIENTID') || 'disabled',
+      clientSecret: configService.get('OIDC_CLIENT_SECRET') || 'disabled',
+      callbackURL: `${configService.get('EXTERNAL_URL')}/authn/oidc_callback`,
+      pkce:
+        configService.get('OIDC_USES_PKCE_S256') === 'true'
+          ? 'S256'
+          : configService.get('OIDC_USES_PKCE_PLAIN') === 'true'
+            ? 'plain'
             : undefined,
-        agent:
-          configService.get('OIDC_USE_HTTPS_PROXY') === 'true'
-            ? new HttpsProxyAgent(configService.get('HTTPS_PROXY') ?? '')
-            : undefined
-      },
-      // using the 9-arity function so that we can access the underlying JSON response and extract the 'email_verified' attribute
-      async (
-        _issuer: string,
-        uiProfile: OIDCProfile,
-        _idProfile: object,
-        _context: object,
-        _idToken: string,
-        _accessToken: string,
-        _refreshToken: string,
-        _params: object,
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        done: any
-      ) => {
-        this.logger.debug('in oidc strategy file');
-        this.logger.debug(JSON.stringify(uiProfile, null, 2));
-        const userData = uiProfile._json;
-        const {given_name, family_name, email, email_verified, groups} =
-          userData;
-        if (
-          configService.get('OIDC_USES_VERIFIED_EMAIL') === 'false' ||
-          email_verified
-        ) {
-          const user = await authnService.validateOrCreateUser(
-            email,
-            given_name,
-            family_name,
-            'oidc'
-          );
+      scope: ['openid', 'email', 'profile'],
+      skipUserProfile: false,
+      proxy:
+        configService.get('OIDC_USE_HTTPS_PROXY') === 'true' ? true : undefined,
+      agent:
+        configService.get('OIDC_USE_HTTPS_PROXY') === 'true'
+          ? new HttpsProxyAgent(configService.get('HTTPS_PROXY') ?? '')
+          : undefined
+    });
+  }
 
-          if (
-            configService.get('OIDC_EXTERNAL_GROUPS') === 'true' &&
-            groups !== undefined
-          ) {
-            await groupsService.syncUserGroups(user, groups);
-          }
+  // using the 9-arity function so that we can access the underlying JSON response and extract the 'email_verified' attribute
+  async validate(
+    _issuer: string,
+    uiProfile: OIDCProfile,
+    _idProfile: object,
+    _context: object,
+    _idToken: string,
+    _accessToken: string,
+    _refreshToken: string,
+    _params: object,
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    done: any
+  ) {
+    this.logger.debug('in oidc strategy file');
+    this.logger.debug(JSON.stringify(uiProfile, null, 2));
+    const userData = uiProfile._json;
+    const {given_name, family_name, email, email_verified, groups} = userData;
+    if (
+      this.configService.get('OIDC_USES_VERIFIED_EMAIL') === 'false' ||
+      email_verified
+    ) {
+      const user = await this.authnService.validateOrCreateUser(
+        email,
+        given_name,
+        family_name,
+        'oidc'
+      );
 
-          return done(null, user);
-        }
-        return done(
-          new UnauthorizedException(
-            'Please verify your name and email with your identity provider before logging into Heimdall.'
-          )
-        );
+      if (
+        this.configService.get('OIDC_EXTERNAL_GROUPS') === 'true' &&
+        groups !== undefined
+      ) {
+        await this.groupsService.syncUserGroups(user, groups);
       }
+
+      return done(null, user);
+    }
+    return done(
+      new UnauthorizedException(
+        'Please verify your name and email with your identity provider before logging into Heimdall.'
+      )
     );
   }
 }
