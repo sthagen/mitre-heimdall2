7caa98135863bc29795c736e925d760b4aea73a4
Amndeep Singh Mann <amann@mitre.org>
2026-03-15 01:23:03 -0400

    okta updated
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/backend/src/authn/okta.strategy.ts
+++ apps/backend/src/authn/okta.strategy.ts
@@ -39,57 +39,52 @@ export class OktaStrategy extends PassportStrategy(Strategy, 'okta') {
     private readonly authnService: AuthnService,
     private readonly configService: ConfigService
   ) {
-    super(
-      {
-        issuer:
-          configService.get('OKTA_ISSUER_URL') ||
-          `https://${configService.get('OKTA_DOMAIN')}` ||
-          'disabled',
-        authorizationURL:
-          configService.get('OKTA_AUTHORIZATION_URL') ||
-          `https://${configService.get('OKTA_DOMAIN') || 'disabled'}/oauth2/v1/authorize`,
-        tokenURL:
-          configService.get('OKTA_TOKEN_URL') ||
-          `https://${configService.get('OKTA_DOMAIN') || 'disabled'}/oauth2/v1/token`,
-        userInfoURL:
-          configService.get('OKTA_USER_INFO_URL') ||
-          `https://${configService.get('OKTA_DOMAIN') || 'disabled'}/oauth2/v1/userinfo`,
-        clientID: configService.get('OKTA_CLIENTID') || 'disabled',
-        clientSecret: configService.get('OKTA_CLIENTSECRET') || 'disabled',
-        callbackURL: `${configService.get('EXTERNAL_URL')}/authn/okta_callback`,
-        scope: ['openid', 'email', 'profile'],
-        skipUserProfile: false,
-        proxy:
-          configService.get('OKTA_USE_HTTPS_PROXY') === 'true'
-            ? true
-            : undefined,
-        agent:
-          configService.get('OKTA_USE_HTTPS_PROXY') === 'true'
-            ? new HttpsProxyAgent(configService.get('HTTPS_PROXY') ?? '')
-            : undefined
-      },
-      // Okta has no concept of a 'verified' email - the account has to have an email address associated with it - which is why we can use the 3-arity function since we don't need access to the underlying JSON response
-      async (
-        _issuer: string,
-        profile: Profile,
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        done: any
-      ) => {
-        this.logger.debug('in okta strategy file');
-        this.logger.debug(JSON.stringify(profile, null, 2));
-        if (profile.emails.length > 0 && profile.emails[0].value) {
-          const user = await authnService.validateOrCreateUser(
-            profile.emails[0].value,
-            profile.name.givenName,
-            profile.name.familyName,
-            'okta'
-          );
-          return done(null, user);
-        }
-        return done(
-          new UnauthorizedException('Incorrect Username or Password')
-        );
-      }
-    );
+    super({
+      issuer:
+        configService.get('OKTA_ISSUER_URL') ||
+        `https://${configService.get('OKTA_DOMAIN')}` ||
+        'disabled',
+      authorizationURL:
+        configService.get('OKTA_AUTHORIZATION_URL') ||
+        `https://${configService.get('OKTA_DOMAIN') || 'disabled'}/oauth2/v1/authorize`,
+      tokenURL:
+        configService.get('OKTA_TOKEN_URL') ||
+        `https://${configService.get('OKTA_DOMAIN') || 'disabled'}/oauth2/v1/token`,
+      userInfoURL:
+        configService.get('OKTA_USER_INFO_URL') ||
+        `https://${configService.get('OKTA_DOMAIN') || 'disabled'}/oauth2/v1/userinfo`,
+      clientID: configService.get('OKTA_CLIENTID') || 'disabled',
+      clientSecret: configService.get('OKTA_CLIENTSECRET') || 'disabled',
+      callbackURL: `${configService.get('EXTERNAL_URL')}/authn/okta_callback`,
+      scope: ['openid', 'email', 'profile'],
+      skipUserProfile: false,
+      proxy:
+        configService.get('OKTA_USE_HTTPS_PROXY') === 'true' ? true : undefined,
+      agent:
+        configService.get('OKTA_USE_HTTPS_PROXY') === 'true'
+          ? new HttpsProxyAgent(configService.get('HTTPS_PROXY') ?? '')
+          : undefined
+    });
+  }
+
+  // Okta has no concept of a 'verified' email - the account has to have an email address associated with it - which is why we can use the 3-arity function since we don't need access to the underlying JSON response
+  async validate(
+    _issuer: string,
+    profile: Profile,
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    done: any
+  ) {
+    this.logger.debug('in okta strategy file');
+    this.logger.debug(JSON.stringify(profile, null, 2));
+    if (profile.emails.length > 0 && profile.emails[0].value) {
+      const user = await this.authnService.validateOrCreateUser(
+        profile.emails[0].value,
+        profile.name.givenName,
+        profile.name.familyName,
+        'okta'
+      );
+      return done(null, user);
+    }
+    return done(new UnauthorizedException('Incorrect Username or Password'));
   }
 }
